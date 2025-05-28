13bb4eb20e063490f42809066bf8ff763498df8a
Youhorng <2023221kean@aupp.edu.kh>
2025-05-28 09:42:30 +0700

    Add model development notebook

--- app/app.py
+++ app/app.py
@@ -1,14 +1,14 @@
-import streamlit as st
+import streamlit as st # type: ignore
 import pandas as pd
-import joblib
+import joblib # type: ignore
 
 # Load your model (adjust path as needed)
 @st.cache_data
 def load_model():
-    return joblib.load("models/xgb_tuned_model.pkl")
+    return joblib.load("../models/xgb_tuned_model.pkl")
 
 model = load_model()
 
-st.title("Loan Default Prediction")
+st.title("🤖 Loan Default Prediction")
 
-st.sidebar.header("Enter Feature Values")
+st.sidebar.header("Enter Customer Information")
